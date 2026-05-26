eb195fa972fa4eca637cf52c82b796a9b0e17d59
abdallamohamed55v-cmyk <abdallamohamed55v@gmail.com>
2026-05-26 20:05:02 +0300

    Update src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChatPageContent from "./pages/ChatPage";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
